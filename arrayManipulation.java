67500eebcc056c3e79d8eb7641cefa6d659e874c
fawwazalifiofarsa <fawwaz.alifio.farsa@gmail.com>
2024-06-23 10:14:29 +0700

    array manipulation done

---
+++ arrayManipulation.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class arrayManipulation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input array size: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Input element for index-"+i+": ");
+            arr[i] = scanner.nextInt();
+        }
+        System.out.print("Target sum: ");
+        int targetSum = scanner.nextInt();
+        List<int[]> pairs = findPairsWithSum(arr, targetSum);
+        System.out.println("Pairs that sum up to the target:");
+        for (int[] pair : pairs) {
+            System.out.print(Arrays.toString(pair));
+        }
+        scanner.close();
+    }
+
+    public static List<int[]> findPairsWithSum(int[] arr, int targetSum) {
+        List<int[]> pairs = new ArrayList<>();
+        Map<Integer, Boolean> numMap = new HashMap<>();
+
+        for (int num : arr) {
+            int complement = targetSum - num;
+            if (numMap.containsKey(complement)) {
+                pairs.add(new int[] {complement, num});
+            }
+            numMap.put(num, true);
+        }
+
+        return pairs;
+    }
+}
